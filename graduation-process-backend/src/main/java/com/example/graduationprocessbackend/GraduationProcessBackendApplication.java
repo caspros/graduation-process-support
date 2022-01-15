4dbf618887ef967a8e7dd93297400a11f4752ace
caspros <caspros97@gmail.com>
2022-01-15 13:15:30 +0100

    initialize backend and frontend

---
+++ graduation-process-backend/src/main/java/com/example/graduationprocessbackend/GraduationProcessBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.graduationprocessbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationProcessBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduationProcessBackendApplication.class, args);
+	}
+
+}
